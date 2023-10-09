885255582f8836f8bfb32561c1053c06ef719ba5
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-08 19:27:52 -0700

    catalog shows red, green, blue

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,25 +13,37 @@ def get_catalog():
 
     # Can return a max of 20 items.
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions FROM global_inventory")).first()
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory")).first()
+
+    small_red_potion = {
+                            "sku": "RED_POTION_0",
+                            "name": "red potion",
+                            "quantity": 1,
+                            "price": 50,
+                            "potion_type": [100, 0, 0, 0], 
+                        }
+    small_green_potion = {
+                            "sku": "GREEN_POTION_0",
+                            "name": "green potion",
+                            "quantity": 1,
+                            "price": 50,
+                            "potion_type": [0, 100, 0, 0],
+                         }
+    small_blue_potion = {
+                            "sku": "BLUE_POTION_0",
+                            "name": "blue potion",
+                            "quantity": 1,
+                            "price": 50,
+                            "potion_type": [0, 0, 100, 0],
+                        }
+
+    potions_for_sale = []
 
     if result.num_red_potions >= 1 :
-        return [
-                {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": 1,
-                    "price": 50,
-                    "potion_type": [100, 0, 0, 0],
-                }
-            ]
-    else :
-        return [
-            {
-                    "sku": "RED_POTION_0",
-                    "name": "red potion",
-                    "quantity": 0,
-                    "price": 0,
-                    "potion_type": [100, 0, 0, 0],
-                }
-        ]
+        potions_for_sale.append(small_red_potion)
+    if result.num_green_potions >= 1 :
+        potions_for_sale.append(small_green_potion)
+    if result.num_blue_potions >= 1 :
+        potions_for_sale.append(small_blue_potion)
+
+    return potions_for_sale
