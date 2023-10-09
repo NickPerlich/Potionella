6886e6bdb93abc3bd38bd83a0b31fb89caafdf4f
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-09 15:27:06 -0700

    one more time

--- src/api/carts.py
+++ src/api/carts.py
@@ -90,8 +90,10 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     with db.engine.begin() as connection:
         carts_result = connection.execute(sqlalchemy.text("SELECT RED_POTION_0, GREEN_POTION_0, BLUE_POTION_0 FROM carts WHERE cart_id = :cart_id"), params).first()
 
+    payment = int(cart_checkout.payment)
+
     params = {
-        'gold': result.gold + int(cart_checkout), 
+        'gold': result.gold + payment, 
         'num_red_potions': result.num_red_potions - carts_result.RED_POTION_0,
         'num_green_potions': result.num_green_potions - carts_result.GREEN_POTION_0,
         'num_blue_potions': result.num_blue_potions - carts_result.BLUE_POTION_0
@@ -101,6 +103,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = :num_red_potions, num_green_potions = :num_green_potions, num_blue_potions = :num_blue_potions, gold = :gold"), params)
 
-    return {"total_potions_bought": int(cart_checkout) % 1, "total_gold_paid": int(cart_checkout)}
+    return {"total_potions_bought": payment % 1, "total_gold_paid": payment}
     
 
