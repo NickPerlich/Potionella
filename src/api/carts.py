1f155d6ebbb5b20c71ce53fbaa08d4a4ccdde037
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-11 11:55:35 -0700

    price increased

--- src/api/carts.py
+++ src/api/carts.py
@@ -85,7 +85,7 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     payment = carts_result.red_potion_0 + carts_result.green_potion_0 + carts_result.blue_potion_0
 
     params = {
-        'gold': result.gold + payment, 
+        'gold': result.gold + payment*50, 
         'num_red_potions': result.num_red_potions - carts_result.red_potion_0,
         'num_green_potions': result.num_green_potions - carts_result.green_potion_0,
         'num_blue_potions': result.num_blue_potions - carts_result.blue_potion_0
@@ -95,6 +95,6 @@ def checkout(cart_id: int, cart_checkout: CartCheckout):
     with db.engine.begin() as connection:
         connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = :num_red_potions, num_green_potions = :num_green_potions, num_blue_potions = :num_blue_potions, gold = :gold"), params)
 
-    return {"total_potions_bought": payment, "total_gold_paid": payment}
+    return {"total_potions_bought": payment*50, "total_gold_paid": payment}
     
 
--- src/api/catalog.py
+++ src/api/catalog.py
@@ -19,21 +19,21 @@ def get_catalog():
                         "sku": "RED_POTION_0",
                         "name": "red potion",
                         "quantity": 1,
-                        "price": 1,
+                        "price": 50,
                         "potion_type": [100, 0, 0, 0], 
     }
     small_green_potion = {
                         "sku": "GREEN_POTION_0",
                         "name": "green potion",
                         "quantity": 1,
-                        "price": 1,
+                        "price": 50,
                         "potion_type": [0, 100, 0, 0],
     }
     small_blue_potion = {
                         "sku": "BLUE_POTION_0",
                         "name": "blue potion",
                         "quantity": 1,
-                        "price": 1,
+                        "price": 50,
                         "potion_type": [0, 0, 100, 0],
     }
 
