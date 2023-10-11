1be5c262ef4cf351515a12984985cd08e2c88196
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-11 07:40:15 -0700

    first attempt at audit

--- src/api/audit.py
+++ src/api/audit.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from src.api import auth
 import math
+import sqlalchemy
+from src import database as db
 
 router = APIRouter(
     prefix="/audit",
@@ -12,8 +14,10 @@ router = APIRouter(
 @router.get("/inventory")
 def get_inventory():
     """ """
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions, num_red_ml, num_green_ml, num_blue_ml, gold FROM global_inventory")).first()
     
-    return {"number_of_potions": 0, "ml_in_barrels": 0, "gold": 0}
+    return {"number_of_potions": result.num_red_potions + result.num_green_potions + result.num_blue_potions, "ml_in_barrels": result.num_red_ml + result.num_green_ml + result.num_blue_ml, "gold": result.gold}
 
 class Result(BaseModel):
     gold_match: bool
