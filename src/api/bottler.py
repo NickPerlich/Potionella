5c9c4c9f4eb86c2ead67e72beaacfaacbf842a0a
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-15 13:15:22 -0700

    debugging bottler /plan and updated bottler /deliver

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -20,56 +20,34 @@ def post_deliver_bottles(potions_delivered: list[PotionInventory]):
     """ """
     print(potions_delivered)
 
-    small_red_potion = None
-    small_green_potion = None
-    small_blue_potion = None
+    ml_spent = [0,0,0,0]
 
-    for potion in potions_delivered:
-        if potion.potion_type == [100, 0, 0, 0]:
-            small_red_potion = potion
-        if potion.potion_type == [0, 100, 0, 0]:
-            small_green_potion = potion
-        if potion.potion_type == [0, 0, 100, 0]:
-            small_blue_potion = potion
-
-    if small_red_potion is not None:
+    if len(potions_delivered) > 0:
+        # add all the potions I bottled to catalog 
+        for potion in potions_delivered:
+            ml_spent = [ml_lost + (ml_cost * potion.quantity) for ml_lost, ml_cost in zip(ml_spent, potion.potion_type)]
+            with db.engine.begin() as connection:
+                connection.execute(sqlalchemy.text("UPDATE catalog \
+                                                    SET quantity = catalog.quantity + :amount \
+                                                    WHERE potion_type = :type"), {
+                                                        'amount': potion.quantity,
+                                                        'type': potion.potion_type
+                                                    })
+        # subtract ml I spent
         with db.engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text("SELECT num_red_ml, num_red_potions FROM global_inventory")).first()
+            connection.execute(sqlalchemy.text("UPDATE inventory \
+                                                SET quantity = CASE \
+                                                    WHEN name = 'red_ml' THEN inventory.quantity - :red \
+                                                    WHEN name = 'green_ml' THEN inventory.quantity - :green \
+                                                    WHEN name = 'blue_ml' THEN inventory.quantity - :blue \
+                                                    WHEN name = 'dark_ml' THEN inventory.quantity - :dark \
+                                                END"), {
+                                                    'red': ml_spent[0],
+                                                    'green': ml_spent[1],
+                                                    'blue': ml_spent[2],
+                                                    'dark': ml_spent[3]
+                                                })
         
-        # subtract red ml and add red potions to Potionella's storage
-        params = {
-            'num_red_potions': result.num_red_potions + small_red_potion.quantity,
-            'num_red_ml': result.num_red_ml - small_red_potion.quantity*100,
-        }
-
-        with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_red_potions = :num_red_potions, num_red_ml = :num_red_ml"), params)
-
-    if small_green_potion is not None:
-        with db.engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text("SELECT num_green_ml, num_green_potions FROM global_inventory")).first()
-        
-        # subtract green ml and add green potions to Potionella's storage
-        params = {
-            'num_green_potions': result.num_green_potions + small_green_potion.quantity,
-            'num_green_ml': result.num_green_ml - small_green_potion.quantity*100,
-        }
-
-        with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = :num_green_potions, num_green_ml = :num_green_ml"), params)
-
-    if small_blue_potion is not None:
-        with db.engine.begin() as connection:
-            result = connection.execute(sqlalchemy.text("SELECT num_blue_ml, num_blue_potions FROM global_inventory")).first()
-        
-        # subtract blue ml and add blue potions to Potionella's storage
-        params = {
-            'num_blue_potions': result.num_blue_potions + small_blue_potion.quantity,
-            'num_blue_ml': result.num_blue_ml - small_blue_potion.quantity*100,
-        }
-
-        with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_blue_potions = :num_blue_potions, num_blue_ml = :num_blue_ml"), params)
 
     return "OK"
 
@@ -87,6 +65,7 @@ def get_bottle_plan():
                                                      JOIN catalog ON bottler.potion_id = catalog.id\
                                                      WHERE bottler.bottle = TRUE \
                                                      ORDER BY bottler.priority ASC")).all()
+    print(result)
         
     with db.engine.begin() as connection:
         inventory = connection.execute(sqlalchemy.text("SELECT quantity \
