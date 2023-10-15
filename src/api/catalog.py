aea55dcac940f826306556da9de051702b64cd14
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-15 13:39:28 -0700

    updated catalog

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -13,37 +13,24 @@ def get_catalog():
 
     # Can return a max of 20 items.
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory")).first()
-
-    small_red_potion = {
-                        "sku": "RED_POTION_0",
-                        "name": "red potion",
-                        "quantity": 1,
-                        "price": 50,
-                        "potion_type": [100, 0, 0, 0], 
-    }
-    small_green_potion = {
-                        "sku": "GREEN_POTION_0",
-                        "name": "green potion",
-                        "quantity": 1,
-                        "price": 50,
-                        "potion_type": [0, 100, 0, 0],
-    }
-    small_blue_potion = {
-                        "sku": "BLUE_POTION_0",
-                        "name": "blue potion",
-                        "quantity": 1,
-                        "price": 50,
-                        "potion_type": [0, 0, 100, 0],
-    }
+        result = connection.execute(sqlalchemy.text("SELECT sku, name, quantity, price, potion_type \
+                                                     FROM inventory \
+                                                     WHERE for_sale = TRUE \
+                                                     ORDER BY priority ASC")).all()
 
     potions_for_sale = []
+    quantity = 0
 
-    if result.num_red_potions >= 1 :
-        potions_for_sale.append(small_red_potion)
-    if result.num_green_potions >= 1 :
-        potions_for_sale.append(small_green_potion)
-    if result.num_blue_potions >= 1 :
-        potions_for_sale.append(small_blue_potion)
+    for row in result:
+        if quantity == 20:
+            break
+        potions_for_sale.append({
+            'sku': row.sku,
+            'name': row.name,
+            'quantity': row.quantity,
+            'price': row.price,
+            'potion_type': row.potion_type
+        })
+        quantity += row.quantity
 
     return potions_for_sale
