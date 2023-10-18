5cd527b72fac74de0677d198c396b8841aab4f36
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-17 18:21:24 -0700

    updating barrels /deliver to interact with transactions and ledger instead of inventory

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -26,26 +26,20 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
     print(barrels_delivered)
 
     gold_spent = 0
-    # add all the ml I bought to inventory 
+    # create transaction and ledger entries for each barrel purchased 
     if len(barrels_delivered) > 0:
         for barrel in barrels_delivered:
-            gold_spent += barrel.price * barrel.quantity
+        # create transaction for each barrel
+            description = f"Potionella bought {barrel.quantity} {barrel.sku} for {barrel.price*barrel.quantity}"
             with db.engine.begin() as connection:
-                connection.execute(sqlalchemy.text("UPDATE inventory \
-                                                    SET quantity = inventory.quantity + :ml_purchased \
-                                                    WHERE potion_type = :type"), {
-                                                        'ml_purchased': barrel.ml_per_barrel * barrel.quantity,
-                                                        'type': barrel.potion_type
-                                                    })
-            print(barrel.ml_per_barrel * barrel.quantity)
-            print(barrel.potion_type)
-        # subtract gold I spent
-        with db.engine.begin() as connection:
-            connection.execute(sqlalchemy.text("UPDATE inventory \
-                                                SET quantity = inventory.quantity - :gold_spent \
-                                                WHERE name = 'gold'"), {
-                                                    'gold_spent': gold_spent
-                                                })
+                result = connection.execute(sqlalchemy.text("INSERT INTO transactions (description) \
+                                                             VALUES (:desc) \
+                                                             RETURNING id"), {
+                                                                 'desc': description
+                                                             })
+                transaction_id = result.scalar()
+        
+        
 
     return "OK"
 
@@ -57,7 +51,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
     
     # from the database, get all barrels I want to buy sorted by priority
     with db.engine.begin() as connection:
-        result = connection.execute(sqlalchemy.text("SELECT sku, quantity \
+        result = connection.execute(sqlalchemy.text("SELECT sku, wish \
                                                      FROM barrels \
                                                      WHERE purchase = TRUE \
                                                      ORDER BY priority ASC")).all()
@@ -74,14 +68,12 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         barrel_for_sale = next((barrel for barrel in wholesale_catalog if barrel.sku == row.sku), None)
         if barrel_for_sale is not None and gold >= barrel_for_sale.price:
             quantity = 0
-            wish = row.quantity
+            wish = row.wish
             while gold >= barrel_for_sale.price and wish > 0:
                 quantity += 1
                 wish -= 1
                 gold -= barrel_for_sale.price 
             barrels_to_purchase.append({ 'sku': row.sku, 'quantity': quantity })
-
-    print(barrels_to_purchase)
 
     return barrels_to_purchase
 
