f9d07c009b0d41a899c1089527e251fc17ddd10a
NickPerlich <nicholasperlich2003@gmail.com>
2023-10-20 11:32:05 -0700

    please

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -49,7 +49,7 @@ def post_deliver_barrels(barrels_delivered: list[Barrel]):
                                                         'delta': barrel.quantity * barrel.ml_per_barrel},
                                                         {
                                                             'trans_id': transaction_id,
-                                                            'p_type': Null,
+                                                            'p_type': null,
                                                             'i_type': 'gold',
                                                             'delta': barrel.quantity * barrel.price
                                                         }])
